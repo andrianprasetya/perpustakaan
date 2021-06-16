0e12b0f86aad746c190dd80e28e2ea19a3153fd4
andrianprasetya <andrianprasetya223@gmail.com>
2021-06-16 18:14:48 +0700

    ganti Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Skripsi Andrian
+                    Sidang Sebentar Lagi
                 </div>
 
                 <div class="links">
