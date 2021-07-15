41c5b1a0e062006b59785c7005a6a2626d404805
andrianprasetya <andrianprasetya223@gmail.com>
2021-07-15 20:54:20 +0700

    test PhpUnit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sidang Sebentar Lagi
+                    Testing lagi
                 </div>
 
                 <div class="links">
