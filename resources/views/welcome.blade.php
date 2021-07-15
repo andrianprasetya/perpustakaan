deda209d4e2b1db886c6bca3a3b50346555616a6
andrianprasetya <andrianprasetya223@gmail.com>
2021-07-15 21:03:05 +0700

    testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Testing lagi
+                    Testing
                 </div>
 
                 <div class="links">
