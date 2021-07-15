50a990768bb5db66e3f1ba33fa950e9ab5b09446
andrianprasetya <andrianprasetya223@gmail.com>
2021-07-15 21:08:07 +0700

    testing apa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Testing
+                    Testing apa
                 </div>
 
                 <div class="links">
