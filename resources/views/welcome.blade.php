7df231250b7f2c7c529f553c967c6e229f0d97b0
andrianprasetya <andrianprasetya223@gmail.com>
2021-07-17 15:46:51 +0700

    test case

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Testing
+                    check test case
                 </div>
 
                 <div class="links">
